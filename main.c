12405f153f7c34385a0a4b3fdc182caf46167d24
Dr-Verdin <piscioneri24122004@gmail.com>
2024-06-09 16:41:33 -0300

    bibliotecas

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 //TODO: criar classe reserva. Os atributos são: nome, sobrenome, cpf, data, numero do voo, id, numero de assento, classe, valor, origem e destino.
 //alguns dados são o mesmo para todos os outros. 
